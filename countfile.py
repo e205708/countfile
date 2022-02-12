8958c087afc1a4327ceb03f65d2c7ef01f6bfbc7
Touyama Hiroto <e205708@ie.u-ryukyu.ac.jp>
2022-02-12 20:54:23 +0900

    add output

--- countfile.py
+++ countfile.py
@@ -2,25 +2,32 @@ import os
 import subprocess
 import re
 
-number_of_lines = 0
-number_of_words = 0
-number_of_bites = 0
-
-check_extension = ".java"
+check_extension = ["java","c","py","go"]
 check_path = "/Users/touyamahiroto/"
 
-def find_file(path):
-    global number_of_lines,number_of_words,number_of_bites
+def find_file(path,_check_extension):
+    number_of_lines = 0
+    number_of_words = 0
+    number_of_bites = 0
+
+    output_file = open(os.getcwd() +'/result/'+ _check_extension +'.txt', 'w')
+
     for pathname, dirnames, filenames in os.walk(path):
         for filename in filenames:
-             if filename.endswith(check_extension):
+             if filename.endswith("."+_check_extension):
+                #print(os.path.join(pathname,filename))
+                output_file.write(os.path.join(pathname,filename) +"\n")
                 proc = subprocess.run(["wc",os.path.join(pathname,filename)],stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                 data = re.findall(r"\d+", proc.stdout)
                 number_of_lines += int(data[0])
                 number_of_words += int(data[1])
                 number_of_bites += int(data[2])
+    print(str(number_of_lines) + " " + str(number_of_words) + " " + str(number_of_bites) + "\n")
+    output_file.write(str(number_of_lines) + " " + str(number_of_words) + " " + str(number_of_bites) + "\n")
 
+    output_file.close()
 
-find_file(check_path)
-print(str(number_of_lines) + " " + str(number_of_words) + " " + str(number_of_bites) + "\n")
+for extn in check_extension:
+    find_file(check_path,extn)
+
 
